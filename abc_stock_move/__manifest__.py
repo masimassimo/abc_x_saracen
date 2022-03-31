13a925095ef679accbb79188df932135e470cdd9
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-03-31 09:19:25 +0200

    Update __manifest__.py

--- abc_stock_move/__manifest__.py
+++ abc_stock_move/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ABC Q.tà Completata sul movimento d'origine",
+    'name': "ABC - abc_stock_move",
 
     'summary': """
         ABC Q.tà Completata sul movimento d'origine""",
@@ -9,26 +9,20 @@
         ABC Q.tà Completata sul movimento d'origine
     """,
 
-    'author': "Massimo Masi",
+    'author': "A.B.C. Srl",
     'website': "https://www.abcstrategie.it",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    
+    'category': 'Stock',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','stock'],
 
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        #'demo/demo.xml',
-    ],
+    'installable': True,
+    'application': True,
+    'demo': [],
 }
