e2a13fa9b13b01492453ee459a7182d6faff7536
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-04-07 18:20:32 +0200

    Add files via upload

--- abc_stock_move/__manifest__.py
+++ abc_stock_move/__manifest__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+# QUESTO MODULO PERMETTE DI VISUALIZZARE SULLA RIGA DI UN MOVIMENTO DI MAGAZZINO
+# LA QUANTITA COMPLETATA NELL'ORDINE DI ORIGINE IN MODO
+# ANCHE SE E' SUPERIORE A QUELLA RICHIESTA
 {
-    'name': "ABC - abc_stock_move",
+    'name': "ABC Q.tà Completata sul movimento d'origine",
 
     'summary': """
         ABC Q.tà Completata sul movimento d'origine""",
@@ -9,20 +12,26 @@
         ABC Q.tà Completata sul movimento d'origine
     """,
 
-    'author': "A.B.C. Srl",
+    'author': "Massimo Masi",
     'website': "https://www.abcstrategie.it",
-    
-    'category': 'Stock',
-    'version': '0.1',
 
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.2',
+
+    # any module necessary for this one to work correctly
     'depends': ['base','stock'],
 
+    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    'installable': True,
-    'application': True,
-    'demo': [],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
 }
