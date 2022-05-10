157799b91b3438720715500433edd6ded7f51465
ABC-Strategie-GitHub <abc.strategie.github@gmail.com>
2022-05-10 15:08:34 +0200

    [UPD]

--- abc_report_purc_ord_no_prices/__manifest__.py
+++ abc_report_purc_ord_no_prices/__manifest__.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "abc_report_purc_ord_no_prices",
+    'name': "Report di Stampa Ordini di Acquisto Senza Prezzi",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Report di Stampa Ordini di Acquisto Senza Prezzi""",
 
     'description': """
-        Long description of module's purpose
+        Report di Stampa Ordini di Acquisto Senza Prezzi
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Massimo Masi",
+    'website': "https://www.abcstrategie.it",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Purchases',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','purchase'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'report/purchase_order_wo_prices.xml',
         'report/purchase_reports.xml',
+        'report/purchasequotation_ext.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
